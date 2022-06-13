e437f29653767ad27823fab34d4aa3275fcb7dc4
Sergixx31 <sergixx31@gmail.com>
2022-06-13 22:39:26 +0200

    add files dia1 and dia2

---
+++ Dia1.py
@@ -0,0 +1,38 @@
+#print("Me encanta estudiar Python.")
+#print('Me encanta "estudiar" Python.')
+#String pk esta entre comillas -- print('100 + 50')
+#print("hola "+"Sergi")
+#print("hola" + " " + "Sergi")
+#print("Me llamo \"Sergi\"")
+#print("Esto es una linea\ny esta es otra linea")
+#\t tabular print("\tEsto es la tercera linea")
+#print("Este signo \\ es una barra doble")
+#/n salto de linea print("Línea 1" + "\nLínea 2" + "\nLínea 3")
+#print("A" + "\tB" + "\tC")
+#print("D" + "\tE" + "\tF")
+#print("G" + "\tH" + "\tI")
+#print("A\tB\tC\nD\tE\tF\nG\tH\tI")
+#print("Barra Normal: " + "/")
+#print("Barra Invertida: " + "\\")
+#print("Tu nombre es " + input("Dime tu nombre: ") + " " + input("Dime tu apellido: "))
+#print(input("¿Qué estás estudiando?\n"))
+#print(input("Escribe tu nombre: ") + " " + input("Escribe tu apellido: "))
+#print("La cerverza se llama \n" + '\"' + input("¿Como se llama tu mascota? ") + " " + input("¿Cual es tu color favorito? ") + '\"')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
