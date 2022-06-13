f5f710294010883317f73efb082eb5ed955f63c2
Sergixx31 <sergixx31@gmail.com>
2022-06-13 22:48:21 +0200

    add more exemples

---
+++ Dia2/Dia2.py
@@ -0,0 +1,22 @@
+#nombre = "Juan"
+#print(nombre)
+#nombre = "Laura"
+#print(nombre)
+#-----------------------------------
+#edad = 30
+#edad2 = 15
+#print(edad + edad2)
+#-----------------------------------
+#nombre = input("dime tu nombre ")
+#print("Tu nombre es " + nombre)
+#-----------------------------------
+#nombre = "hola "
+#nombre2 = "Python"
+#frase = nombre + nombre2
+#print(frase)
+#-----------------------------------
+#edad = 30
+#edad2 = 50
+#frase = edad + edad2
+#print(frase)
+#-----------------------------------
