d01b7f2c0abc39ffdc2318ad554c76c8a733a1b3
Sergixx31 <sergixx31@gmail.com>
2022-06-14 23:25:30 +0200

    add index dia 3

---
+++ Dia3/metodosString.py
@@ -0,0 +1,86 @@
+#poner todo el texto en mayuscula
+#texto = "Este es el texto de Sergi"
+#resultado = texto.upper()
+#print(resultado)
+
+#se le indica que posicion va en mayuscula
+#texto = "Este es el texto de Sergi"
+#resultado = texto[2].upper()
+#print(resultado)
+
+#se le indica que posicion va en minuscula
+#texto = "Este es el texto de Sergi"
+#resultado = texto.lower()
+#print(resultado)
+
+#te crea una lista separando las palabras, si le añadimos un aprametro como la "t", coje de t a t juntandolas
+#texto = "Este es el texto de Sergi"
+#resultado = texto.split("t")
+#print(resultado)
+
+
+#a = "aprender"
+#b = "python"
+#c = "es"
+#d = "genial"
+#e = " ".join([a,b,c,d])
+#print(e)
+
+
+#es un index, busca en que posicion está, pero si le pones algo que no existe, no peta
+#simplemente te saldra -1
+#texto = "Este es el texto de Sergi"
+#resultado = texto.find("t")
+#print(resultado)
+
+#basicamente reemplaza
+#texto = "Este es el texto de Sergi"
+#resultado = texto.replace("Sergi", "Aina")
+#print(resultado)
+
+
+
+
+#frase = "Especialmente en las comunicaciones electrónicas, la escritura enteramente en mayúsculas equivale a gritar."
+#resultado = frase.upper()
+#print(resultado)
+
+
+#lista_palabras = ["La","legibilidad","cuenta."]
+#resultado = " ".join(lista_palabras)
+#print(resultado)
+
+
+frase = "Si la implementación es difícil de explicar, puede que sea una mala idea."
+reemplazar = frase.replace("difícil","fácil")
+reemplazar2 = reemplazar.replace("mala","buena")
+print(reemplazar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
