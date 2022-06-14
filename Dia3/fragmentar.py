d5971cebe3be0138d2c0b79b3d953d178ee0c0cc
Sergixx31 <sergixx31@gmail.com>
2022-06-14 22:54:08 +0200

    add index dia 3

---
+++ Dia3/fragmentar.py
@@ -0,0 +1,35 @@
+#texto = "ABCDEFGHIJKLM"
+#fragmento = texto[2:5]
+#print(fragmento)
+
+#desde el indice 2, hasta el final
+#texto = "ABCDEFGHIJKLM"
+#fragmento = texto[2:]
+#print(fragmento)
+
+#desde el indice 0, hasta el 5
+#texto = "ABCDEFGHIJKLM"
+#fragmento = texto[:5]
+#print(fragmento)
+
+#salteando de 1 en 1, siempre 1 menos
+#texto = "ABCDEFGHIJKLM"
+#fragmento = texto[2:10:2]
+#print(fragmento)
+
+#tambien funciona con negativos, pero va al reves
+#texto = "ABCDEFGHIJKLM"
+#fragmento = texto[::-2]
+#print(fragmento)
+
+#frase = "Controlar la complejidad es la esencia de la programación"
+#fragmento = frase[0:9]
+#print(fragmento)
+
+#frase = "Nunca confíes en un ordenador que no puedas lanzar por una ventana"
+#fragmento = frase[8::3]
+#print(fragmento)
+
+#frase = "Es genial trabajar con ordenadores. No discuten, lo recuerdan todo y no se beben tu cerveza"
+#fragmento = frase[::-1]
+#print(fragmento)
