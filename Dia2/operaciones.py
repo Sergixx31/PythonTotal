687d556ff0eb065213a8d002dd2dedc09f54d6d0
smartinez <smartinez@zitrointeractive.com>
2022-06-14 14:10:27 +0200

    add more files

---
+++ Dia2/operaciones.py
@@ -0,0 +1,30 @@
+x = 6
+y = 2
+z = 7
+
+print(f"{x} mas {y} es igual a {x+y}")
+print(f"{x} menos {y} es igual a {x-y}")
+print(f"{x} por {y} es igual a {x*y}")
+print(f"{x} dividido {y} es igual a {x/y}")
+
+#elimina los deciamles
+print(f"{z} dividido al piso de {y} es igual a {z//y}")
+
+#saca el residuo de la division
+print(f"{z} modulo de {y} es igual {z%y}")
+
+#exponer un numero, x elevado a y
+print(f"{x} elevado a la {y} es igual a {x**y}")
+
+#exponer un numero, x elevado a 3, tambien se pueden poner numeros donde las variables
+print(f"{x} elevado a la {3} es igual a {x**3}")
+
+#raiz cuadrada, el numero que quieras **0.5
+print(f"La raiz cuadrada de {x} es {x**0.5}")
+
+
+
+#al piso print(f"{874//27}")
+#extracto print(f"{456%33")
+#raiz cuadrada print(f"{783**0.5}")
+
