50ab3a608bc6266b5f4c5fc155b913f58b8d6f52
smartinez <smartinez@zitrointeractive.com>
2022-06-14 16:24:27 +0200

    add more files

--- Dia2/PROYECTO.py
+++ Dia2/PROYECTO.py
@@ -2,5 +2,5 @@ nombre_usuario = input("Dime tu nombre: ")
 ventas = float(input("Cuantas ventas has obtenido: "))
 porcentaje_ventas = round(ventas * 13 / 100,2)
 
-print(f"Ok {nombre_usuario}. Este mes ganaste ${porcentaje_ventas}")
+print(f"Ok {nombre_usuario}. Ganas de comision un 13%, te tocan ${porcentaje_ventas}")
 
