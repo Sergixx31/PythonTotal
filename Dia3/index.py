be082d3a90e3eb3d99afa6afac94c27856d9e726
Sergixx31 <sergixx31@gmail.com>
2022-06-14 20:57:18 +0200

    add index dia 3

---
+++ Dia3/index.py
@@ -0,0 +1,27 @@
+#mi_texto = "Esta es una prueba"
+#resultado = mi_texto[9]
+#print(resultado)
+
+#mi_texto = "Esta es una prueba"
+#resultado = mi_texto[-4]
+#print(resultado)
+
+#donde empieza la palabra, la 12, la p
+#mi_texto = "Esta es una prueba"
+#resultado = mi_texto.index("prueba")
+#print(resultado)
+
+#error, pk la mayuscula no existe
+#mi_texto = "Esta es una prueba"
+#resultado = mi_texto.index("P")
+#print(resultado)
+
+#busca la a, a partir del 5 hasta el 11
+#mi_texto = "Esta es una prueba"
+#resultado = mi_texto.index("a", 5, 11)
+#print(resultado)
+
+#busca de derecha a izquierda, igual que el index normal sin r
+#mi_texto = "Esta es una prueba"
+#resultado = mi_texto.rindex("a")
+#print(resultado)
