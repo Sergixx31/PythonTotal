3833393272b084f85c4634919bb21941f765c051
smartinez <smartinez@zitrointeractive.com>
2022-06-14 13:40:06 +0200

    add more files

---
+++ Dia2/conversiones.py
@@ -0,0 +1,47 @@
+#conversion implicita (automatica)
+#num1 = 20
+#num2 = 30.5
+#num1 = num1 + num2
+#print(type(num1))
+#print(type(num2))
+#elimina los decimales, pasa de float a integer
+#num1 = 5.8
+#print(num1)
+#print(type(num1))
+#num2 = int(num1)
+#print(num2)
+#print(type(num2))
+#ESTO DA ERROR
+#edad = input("dime tu edad: ")
+#edad = int(edad)
+#nueva_edad = 1 + edad
+#print("tu nueva edad es: " + nueva_edad)
+#num1 = 7.5
+#num1 = int(num1)
+#print(type(num1))
+#num2 = 10
+#num2 = float(num2)
+#print(type(num2))
+#num1 = "7.5"
+#num2 = "10"
+#print(float(num1) + int(num2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Dia2/formatear_cadenas.py
@@ -0,0 +1,36 @@
+#x = 10
+#y = 5
+#antigua forma
+#print("Mis numeros son " + str(x) + " y " + str(y))
+
+#nueva manera
+#print("mis numeros son {} y {}".format(x,y))
+#encima puedes hacer la suma al final LOL
+#print("La suma de {} y {} es igual a {}".format(y,x,y+x))
+
+#nueva manera, mas buena, legible, usar esta!!
+#color = "rojo"
+#marticula = 341245
+#print(f"El auto es {color} y su marticula es {marticula}")
+
+
+#nombre_asociado = "Jesse Pinkman"
+#numero_asociado = 399058
+#print(f"Estimado/a {nombre_asociado}, su número de asociado es: {numero_asociado}")
+
+#puntos_nuevos = 350
+#puntos_totales = 1225
+#print(f"Has ganado {puntos_nuevos} puntos! En total, acumulas {puntos_totales} puntos")
+
+
+#puntos_anteriores = 875
+#puntos_nuevos = 350
+#puntos_totales = puntos_anteriores + puntos_nuevos
+#print(f"Has ganado {puntos_nuevos} puntos! En total, acumulas {puntos_totales} puntos")
+
+
+
+
+
+
+
