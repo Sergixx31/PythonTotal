fe06559a0a37a5ca724308eeeea51db9d37c770c
Sergixx31 <sergixx31@gmail.com>
2022-06-15 19:44:46 +0200

    add index dia 3

---
+++ Dia3/diccionario.py
@@ -0,0 +1,90 @@
+#diccionario = {"c1":"valor1", "c2":"valor2"}
+#print(diccionario)
+
+#diccionario = {"c1":"valor1", "c2":"valor2"}
+#resultado = diccionario["c1"]
+#print(resultado)
+
+#cliente = {"nombre":"juan","apellido":"Fuentes","peso":88,"talla":1.76}
+#consulta = (cliente["talla"])
+#print(consulta)
+
+#imprime lo que sea que haya en el clave c2, pero el indice 1
+#dic = {"c1":55,"c2":[10,20,30],"c3":{"s1":100,"s2":200}}
+#print(dic["c2"][1])
+
+#dic = {"c1":["a","b","c"],"c2":["d","e","f"]}
+#print(dic["c2"][1].upper())
+
+
+#dic = {1:"a",2:"b"}
+#print(dic)
+#agregar valor a un diccionario ya creado
+#dic[3] = "c"
+#print(dic)
+
+#saber todas la claves que tiene
+#print(dic.keys())
+
+#saber todas los valores que tiene
+#print(dic.values())
+
+#saber todas los items que tiene
+#print(dic.items())
+
+
+#nombre: Karen
+#apellido: Jurgens
+#edad: 35
+#ocupacion: Periodista
+
+
+
+#mi_dic = {"nombre":"Karen", "apellido":"Jurgens","edad":35,"ocupacion":"Periodista"}
+
+
+#mi_dict = {"valores_1":{"v1":3,"v2":6},"puntos":{"points1":9,"points2":[10,300,15]}}
+#print(mi_dict["puntos"]["points2"][1])
+
+
+#mi_dic = {"nombre":"Karen", "apellido":"Jurgens", "edad":35, "ocupacion":"Periodista"}
+#mi_dic["edad"] = 36
+#mi_dic["pais"] = "Colombia"
+#mi_dic["ocupacion"] = "Editora"
+#print(mi_dic)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
