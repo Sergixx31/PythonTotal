b3fd93e559fcb1500dc112b262b47b86e6d8cdba
Sergixx31 <sergixx31@gmail.com>
2022-06-15 23:18:10 +0200

    add boolean y sets dia 3

---
+++ Dia3/booleanos.py
@@ -0,0 +1,30 @@
+#var1 = True
+#var2 = False
+#print(type(var1))
+#print(var1)
+
+#== >= <= != > <
+#numero = 5 > 2 +3
+#print(type(numero))
+#print(numero)
+
+#numero = bool(5<6)
+#print(type(numero))
+#print(numero)
+
+#lista = [1,2,3,4]
+#control = 5 in lista
+#not in
+#print(type(control))
+#print(control)
+
+
+#prueba = 5 > 10
+#print(prueba)
+
+#division = 17834 / 34
+#multi = 87 * 56
+#print(division > multi)
+
+#raiz = 25**0.5
+#print(raiz == 5)
