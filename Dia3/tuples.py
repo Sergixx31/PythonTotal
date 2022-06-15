e7c636542d922c823b47dcd3adf089e2228b5f3d
Sergixx31 <sergixx31@gmail.com>
2022-06-15 19:54:57 +0200

    add diccionarios y tuples dia 3

---
+++ Dia3/tuples.py
@@ -0,0 +1,33 @@
+#mi_tuple = (1,2,3,4)
+#t = (5,5.6,"hola")
+
+
+#i_tuple = (1,2,(10,20),4)
+#print(mi_tuple[2][0])
+
+
+#mi_tuple = (1,2,(10,20),4)
+#mi_tuple = list(mi_tuple)
+#print(type(mi_tuple))
+
+
+#t = (1,2,3)
+#x,y,z = t
+#print(x,y,z)
+
+#t = (1,2,3,1)
+#print(t.count(1))
+#print(t.index(2))
+
+
+
+
+
+#mi_tupla = (1, 2, 3, 2, 3, 1, 3, 2, 3, 3, 3, 1, 3, 2, 2, 1, 3, 2)
+#print(mi_tupla.count(2))
+
+#mi_tupla = (1, 2, 3, 2, 3, 1, 3, 2)
+#mi_lista = list(mi_tupla)
+
+#mi_tupla = (1, 2, 3, 4)
+#a,b,c,d = mi_tupla
