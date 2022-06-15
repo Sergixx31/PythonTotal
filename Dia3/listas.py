a72586fde9c713d2519ac59f4dd87b453c25b37a
smartinez <smartinez@zitrointeractive.com>
2022-06-15 14:35:56 +0200

    add more files dia 3

---
+++ Dia3/listas.py
@@ -0,0 +1,57 @@
+#otra_lista = ["hola", 55, 6.1]
+#mi_lista = ["a", "b", "c"]
+#mi_lista2 = ["d", "e", "f"]
+#mi_lista3 = mi_lista + mi_lista2
+
+#se le agrega un elemento mas
+#mi_lista3.append("g")
+
+#eliminado = mi_lista3.pop(0)
+#mi_lista3[0] = "alfa"
+#resultado = mi_lista[0:]
+#print(mi_lista3)
+#print(eliminado)
+
+#ordena alfabeticamente
+#lista = ["g", "o", "b", "m", "c"]
+#lista.sort()
+#print(lista)
+
+#igual que sort, pero del reves
+#lista = ["g", "o", "b", "m", "c"]
+#lista.reverse()
+#print(lista)
+
+
+#mi_lista = ["hola", 3, 4, "Sergi", 50]
+
+#medios_transporte = ["avión", "auto", "barco", "bicicleta"]
+#medios_transporte.append("motocicleta")
+#print(medios_transporte)
+
+
+#frutas = ["manzana", "banana", "mango", "cereza", "sandía"]
+#eliminado = frutas.pop(2)
+#print(eliminado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
