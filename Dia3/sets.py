3f910589751dbe775e7bb430d83fb044913da5ea
Sergixx31 <sergixx31@gmail.com>
2022-06-15 20:26:13 +0200

    add boolean y sets dia 3

---
+++ Dia3/sets.py
@@ -0,0 +1,69 @@
+#mi_set = set((1,2,3,4,5))
+#print(type(mi_set))
+#print(mi_set)
+
+#otra manera de crear un set
+#otro_set = {1,2,3}
+#print(type(otro_set))
+#print(otro_set)
+
+#peta, pk no tienen posiciones
+#print(mi_set[0])
+
+#print(len(mi_set))
+#print(2 in mi_set)
+
+#s1 = {1,2,3}
+#s2 = {3,4,5}
+#s3 = s1.union(s2)
+#print(s3)
+#s1.add(4)
+#s1.remove(3)
+#s1.discard(6)
+#s1.pop()
+
+#vacia el set
+#1.clear()
+#print(s1)
+
+#mi_set_1 = {1, 2, "tres", "cuatro"}
+#mi_set_2 = {"tres", 4, 5}
+#mi_set_3 = mi_set_1.union(mi_set_2)
+#print(mi_set_3)
+
+
+#sorteo = {"Camila", "Margarita", "Axel", "Jorge", "Miguel", "Mónica"}
+#sorteo.pop()
+#print(sorteo)
+
+
+#sorteo = {"Camila", "Margarita", "Axel", "Jorge", "Miguel", "Mónica"}
+#sorteo.add("Damián")
+#print(sorteo)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
