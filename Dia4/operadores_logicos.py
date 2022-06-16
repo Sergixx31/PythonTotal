41ddbde0a67269f9fcab71d05bfe749ee8ff01f6
Sergixx31 <sergixx31@gmail.com>
2022-06-16 21:49:08 +0200

    add day 4 comparaciones

---
+++ Dia4/operadores_logicos.py
@@ -0,0 +1,34 @@
+#and
+#or
+
+#tienen que ser true las dos para que salga true
+#mi_bool = 4 < 5 and 5 > 6
+#mi_bool = (55 == 55) and ("perro" == "perro")
+
+#si una es mentira no pasa nada, devuelve la que sea verdadera
+#mi_bol = 10 == 10 or 3 == 3
+
+#texto = "esta frase es breve"
+#mi_bool = ("frase" in texto) and ("breve" in texto)
+#print(mi_bool)
+
+#nos devuelve basicamente lo contrario
+#mi_bool = not "a" == "a"
+#print(mi_bool)
+
+#num1 =  36
+#num2 = 72 / 2
+#num3 = 48
+#mi_bool = num1 > num2 and num1 < num3
+
+
+#num1 =  36
+#num2 = 72 / 2
+#num3 = 48
+#mi_bool = num1 > num2 or num1 < num3
+
+#palabra1 = "éxito"
+#palabra2 = "tecnología"
+#frase = "Cuando algo es lo suficientemente importante, lo haces incluso si las probabilidades de que salga bien no te acompañan"
+#mi_bool = palabra1 and palabra2 not in frase
+#print(mi_bool)
