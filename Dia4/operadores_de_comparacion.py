cfc85e648f709e96df0e4d0be418dffff526bbe2
Sergixx31 <sergixx31@gmail.com>
2022-06-16 21:10:10 +0200

    add day 4 comparaciones

--- Dia4/operadores_de_comparacion.py
+++ Dia4/operadores_de_comparacion.py
@@ -1,4 +1,4 @@
-#== COMPARACION
+##== COMPARACION
 #=
 #><=
 #><
@@ -18,7 +18,8 @@
 #mi_bool = num1 == num2
 #print(mi_bool)
 
-num1 = 64 * 3
-num2 = 24 * 8
-mi_bool = num1 != num2
-print(mi_bool)
+#num1 = 64 * 3
+#num2 = 24 * 8
+#mi_bool = num1 != num2
+#print(mi_bool)
+
