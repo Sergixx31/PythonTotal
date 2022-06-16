263b6ba43372eb8650ec28ac5058aa8068a5d6d3
smartinez <smartinez@zitrointeractive.com>
2022-06-16 14:55:31 +0200

    add proyecto dia 3

--- Dia4/operadores_de_comparacion.py
+++ Dia4/operadores_de_comparacion.py
@@ -1,7 +1,24 @@
 #== COMPARACION
 #=
-
+#><=
+#><
 #mi_bool = 10==25
 #mi_bool = 5+5==18-8
-mi_bool = "blanco"=="blanco"
+#mi_bool = "blanco"!="blanco"
+#mi_bool = "blanco"=="blanco"
+#print(mi_bool)
+
+#num1 = 36
+#num2 = 17
+#mi_bool = num1 >= num2
+#print(mi_bool)
+
+#num1 = 25**0.5
+#num2 = 5
+#mi_bool = num1 == num2
+#print(mi_bool)
+
+num1 = 64 * 3
+num2 = 24 * 8
+mi_bool = num1 != num2
 print(mi_bool)
