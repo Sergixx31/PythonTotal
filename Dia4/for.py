a1e029fd64b8a894c966e1ff0d54c82857d74550
Sergixx31 <sergixx31@gmail.com>
2022-06-18 14:07:53 +0200

    add day 4 flujo

---
+++ Dia4/for.py
@@ -0,0 +1,106 @@
+#lista = ["a", "b", "c"]
+#for letra in lista:
+#    print(letra)
+
+
+
+#lista = ["a", "b", "c", "d"]
+#for l in lista:
+#    numero_letra = lista.index(l) + 1
+#    print(f"Letra {numero_letra}: {l}")
+
+
+#lista = ["pablo", "laura", "luis", "fede", "julia"]
+##comienza con
+#for nombre in lista:
+#    if nombre.startswith("l"):
+#        print(nombre)
+#    else:
+#        print("nombre que no comienza con L")
+
+
+#numeros = [1,2,3,4,5]
+#mi_valor = 0
+#for numero in numeros:
+#    mi_valor = mi_valor + numero
+    #print(mi_valor)
+#print(mi_valor)
+
+
+#palabra = "python"
+
+#for letra in palabra:
+#    print(letra)
+
+#dic = {"clave1":"a", "clave2":"b", "clave3":"c"}
+
+#se imprime el item completo
+#solo valores, values
+#for item in dic.items():
+#    print(item)
+
+
+
+#alumnos_clase = ["María", "José", "Carlos", "Martina", "Isabel", "Tomás", "Daniela"]
+#for nombre in alumnos_clase:
+#    print("Hola " + nombre)
+
+#lista_numeros = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
+#suma_numeros = 0
+#
+#for numero in lista_numeros:
+#    suma_numeros = suma_numeros + numero
+#
+#print(suma_numeros)
+
+lista_numeros = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
+suma_pares =
+suma_impares =
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
