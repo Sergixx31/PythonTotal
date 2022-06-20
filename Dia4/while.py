0f0ea6c67a102230854e2c40cf640d46ec46a8cb
smartinez <smartinez@zitrointeractive.com>
2022-06-20 14:16:25 +0200

    add loop while

---
+++ Dia4/while.py
@@ -0,0 +1,57 @@
+# monedas = 5
+#
+# while monedas > 0:
+#    print(f"Tengo {monedas} monedas")
+#    monedas -= 1
+# else:
+#    print("no tengo mas dinero")
+
+# respuesta = "s"
+#
+# while respuesta == "s":
+#    respuesta = input("quieres seguir? (s/n)")
+# else:
+#    print("Gracias")
+
+
+# respuesta = "s"
+
+# while respuesta == "s":
+#    pass
+# print("hola")#
+
+
+# nombre = input("Tu nombre: ")
+# for letra in nombre:
+#    if letra == "r":
+#        break
+#    print(letra)
+
+
+# nombre = input("Tu nombre: ")
+# for letra in nombre:
+#    if letra == "r":
+#        continue
+#    print(letra)
+
+
+# numero = 10
+# hile numero >= 0:
+#   print(numero)
+#   numero -= +1
+
+#SIN TERMINAR EL PROBLEMA :/
+#numero = 50
+#while numero >= 0:
+#    if numero % 5 == 0:
+#        print(numero)
+#        numero -= +1
+#    else:
+#        continue
+
+
+
+
+
+
+
