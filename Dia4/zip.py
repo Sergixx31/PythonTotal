a138387ea5092e7239e9c15df9b080b2a8255d94
Sergixx31 <sergixx31@gmail.com>
2022-06-21 20:44:04 +0200

    add day 4 zip

---
+++ Dia4/zip.py
@@ -0,0 +1,55 @@
+#nombres = ["Ana", "Hugo", "Valeria"]
+#edades = [65,29,42]
+#ciudades = ["Lima", "Madrid", "Mexico"]
+##no se puede imprimir un zip, hay que pasarlo a "lista"
+#combinados = list(zip(nombres,edades, ciudades))
+#for nombre, edad, ciudad in combinados:
+#    print(f"{nombre} tiene {edad} años y vive en {ciudad}")
+
+
+#capitales = ["Berlín", "Tokio", "París", "Helsinki", "Ottawa", "Canberra"]
+#paises = ["Alemania", "Japón", "Francia", "Finlandia", "Canadá", "Australia"]
+#
+#combinados = list(zip(capitales,paises))
+#for capitales, paises in combinados:
+#    print(f"La capital de {paises} es {capitales}")
+
+
+#marcas = ["FootLocker", "Bershka"]
+#productos = ["Zapatos", "Ropa"]
+#
+#mi_zip = zip(marcas,productos)
+
+
+#numero1 = ["uno","um","one"]
+#numero2 = ["dos" , "dois" , "two"]
+#numero3 = ["tres" , "três" , "three"]
+#numero4 = ["cuatro" , "quatro" , "four"]
+#numero5 = ["cinco" , "cinco" , "five"]
+#
+#mi_zip = zip(numero1,numero2,numero3,numero4,numero5)
+#
+#for numeros in mi_zip:
+#    print(numeros)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
