368a2bbd6495e2371f7a50d25d56f6c420c699a3
Sergixx31 <sergixx31@gmail.com>
2022-06-21 21:34:35 +0200

    add day 4 minmax

---
+++ Dia4/minymax.py
@@ -0,0 +1,11 @@
+#menor = min(58,96,72,64,25)
+#mayor = max(58,96,72,64,25)
+
+
+#lista = [58,96,72,64,25]
+#print(f"EL menor es {min(lista)} y el mayor es {max(lista)}")
+#print(max(lista)
+
+nombre = "carlos"
+#nombre = ["juan", "pablo", "alicia", "carlos"]
+print(min(nombre))
