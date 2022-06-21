2dcd9736dedb84db87ec263f2c85d6aff8c93efd
smartinez <smartinez@zitrointeractive.com>
2022-06-21 17:17:12 +0200

    add numerator exercise

--- Dia4/enumerador.py
+++ Dia4/enumerador.py
@@ -14,3 +14,28 @@
 #mis_tuples = list(enumerate(lista))
 #print(mis_tuples[1][0])
 
+
+#lista_nombres = ["Marcos", "Laura", "Mónica", "Javier", "Celina", "Marta", "Darío", "Emiliano", "Melisa"]
+#indice = 0
+#for item in lista_nombres:
+#    print(f'{item} se encuentra en el índice {indice}')
+#    indice += 1
+
+
+#mi_tuple = ("Python")
+#lista_indices = list(enumerate(mi_tuple))
+#print(lista_indices)
+
+
+
+lista_nombres = ["Marcos", "Laura", "Mónica", "Javier", "Celina", "Marta", "Darío", "Emiliano", "Melisa"]
+
+
+
+
+
+
+
+
+
+
