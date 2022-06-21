58c67ed2797a935901f277b3a141ef467cb3a3f9
smartinez <smartinez@zitrointeractive.com>
2022-06-21 09:40:57 +0200

    add range file

--- Dia4/rango.py
+++ Dia4/rango.py
@@ -1,1 +1,18 @@
-a
+#no la necesitamos lista = [1,2,3,4]
+
+#el primero es desde donde hasta donde
+#for numero in range(20,31#):
+#    print(numero)
+
+#tercer valor, los saltos#
+#for numero in range(20,31,2):
+#    print(numero)
+
+#lista = list(range(1,101))
+#print(lista)
+
+#mi_lista = list(range(2500,2586))
+#print(mi_lista)#
+
+#mi_lista = list(range(3,301,3))
+#print(mi_lista)#
