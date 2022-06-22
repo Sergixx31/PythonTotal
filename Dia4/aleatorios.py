53beb5df7b7ae6114d3760d8a2347c9dfa2364d6
smartinez <smartinez@zitrointeractive.com>
2022-06-22 15:21:32 +0200

    add numerator exercise and random file

---
+++ Dia4/aleatorios.py
@@ -0,0 +1,40 @@
+#from random import *
+
+#aleatorio = randint(1,50)
+#aleatorio = round(uniform(1,5),1)
+#aleatorio = random()
+
+#colores = ["azul", "rojo", "verde", "amarillo"]
+#aleatorio = choice(colores)
+#print(aleatorio)
+
+#numeros = list(range(5,50,5))
+#shuffle(numeros)
+#print(numeros)
+
+
+#from random import randint
+#aleatorio = randint(1,10)
+#print(aleatorio)
+
+#from random import random
+#aleatorio = random()
+#print(aleatorio)
+
+
+#from random import choice
+#nombres = ["Carlos", "Julia", "Nicole", "Laura", "Mailen"]
+#sorteo = choice(nombres)
+#print(sorteo)
+
+
+
+
+
+
+
+
+
+
+
+
