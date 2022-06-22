b6c5ba40089f5836517c06c15b1c66e57d1f3607
smartinez <smartinez@zitrointeractive.com>
2022-06-22 14:34:38 +0200

    add numerator exercise and random file

--- Dia4/minymax.py
+++ Dia4/minymax.py
@@ -6,6 +6,40 @@
 #print(f"EL menor es {min(lista)} y el mayor es {max(lista)}")
 #print(max(lista)
 
-nombre = "carlos"
+#nombre = "carlos"
 #nombre = ["juan", "pablo", "alicia", "carlos"]
-print(min(nombre))
+#print(min(nombre))
+
+#dic = {"C1":45, "C2":11}
+#print(min(dic.values()))
+
+
+#lista_numeros = [44542247/2, 21310/5, 2134747*33, 44556475, 121676, 6654067, 353254, 123134, 55**12, 611**5]
+#valor_maximo = max(lista_numeros)
+
+
+#lista_numeros = [44542247, 21310, 2134747, 44556475, 121676, 6654067, 353254, 123134, 552512, 611665]
+#rango = max(lista_numeros) - min(lista_numeros)
+#print(rango)
+
+#diccionario_edades = {"Carlos":55, "María":42, "Mabel":78, "José":44, "Lucas":24, "Rocío":35, "Sebastián":19, "Catalina":2,"Darío":49}
+#edad_minima = min(diccionario_edades.values())
+#ultimo_nombre = max(diccionario_edades.keys())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
