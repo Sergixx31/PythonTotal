fa73fbd60b5bc519a52f0d8e8ceffbe320774dfd
smartinez <smartinez@zitrointeractive.com>
2022-06-22 15:40:23 +0200

    add comprension file

---
+++ Dia4/comprension_de_listas.py
@@ -0,0 +1,38 @@
+#palabra = "python"
+
+#lista = [letra for letra in "python"]
+#lista = [n if n * 2 > 10 else "no" for n in range(0,21,2)]
+#lista = [letra for letra in palabra]
+
+#for letra in palabra:
+    #agregar valor
+    #lista.append(letra)
+#print(lista)
+
+
+#pies = [10,20,30,40,50]
+#metros = [p * 3.281 for p in pies]
+#print(metros)
+
+
+#valores = [1, 2, 3, 4, 5, 6, 9.5]
+#valores_cuadrado = [v ** 2 for v in valores]
+#print(valores_cuadrado)
+
+#valores = [1, 2, 3, 4, 5, 6, 9.5]
+
+#valores_pares = [v for v in valores if v % 2 == 0]
+#print(valores_pares)#
+
+
+#temperatura_fahrenheit = [32, 212, 275]
+#grados_celsius = [(g - 32) * (5 / 9) for g in temperatura_fahrenheit]
+#print(grados_celsius)
+
+
+
+
+
+
+
+
