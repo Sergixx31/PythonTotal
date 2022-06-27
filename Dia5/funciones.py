5872eec5ab225a711d909d06d730e207fcf1222b
smartinez <smartinez@zitrointeractive.com>
2022-06-27 13:34:04 +0200

    add file return and funciones

---
+++ Dia5/funciones.py
@@ -0,0 +1,25 @@
+#def saludar_persona(nombre):
+
+    #esta funcion sirve para saludar a las personas
+    #otra linea
+
+    #o así
+
+#   print("Hola " + nombre)
+
+#saludar_persona("Fernando")
+
+#def saludar():
+#    print("¡Hola mundo!")
+
+
+#nombre_persona = "Lucas"
+#def bienvenida(nombre_persona):
+#    print("¡Bienvenido " + nombre_persona + "!")
+
+#un_numero = 10
+#def cuadrado(un_numero):
+#    print(un_numero**2)
+
+
+
