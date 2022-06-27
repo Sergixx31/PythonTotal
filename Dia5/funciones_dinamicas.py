f676dba4f78c78eaf564bb3f3ca79f3d7d98b00b
smartinez <smartinez@zitrointeractive.com>
2022-06-27 15:00:07 +0200

    add file funciones_dinamicas

---
+++ Dia5/funciones_dinamicas.py
@@ -0,0 +1,89 @@
+#def chequear_3_cifras(numero):
+#   return numero in range(100,1000)
+
+#suma = 432 + 321
+
+#suma
+#resultado = chequear_3_cifras(232)
+#print(resultado)
+
+#def chequear_3_cifras(lista):
+#    lista_3_cifras = []
+#
+#    for n in lista:
+#        if n in range(100,1000):
+#            lista_3_cifras.append(n)
+#        else:
+#            pass
+#    return lista_3_cifras
+#
+#resultado = chequear_3_cifras([555,99,600])
+#print(resultado)
+
+
+
+#def todos_positivos():
+#    lista_numeros = [1,2,3,4]
+#    for n in lista_numeros:
+#        if n <= 0:
+#            return False
+#        else:
+#            pass
+#    return True
+#
+#print(todos_positivos())
+
+
+
+#lista_numeros = [1,2,3,5000]
+#
+#def suma_menores():
+#    for n in lista_numeros():
+#        if n > 1 and n < 1000:
+#            suma = n + n
+#            print(suma)
+#        else:
+#            pass
+#        print("a")
+#    print("b")
+#
+#suma_menores(lista_numeros)
+
+
+
+#def cantidad_pares(pares):
+#    cantidad = 0
+#    for n in pares:
+#        if n % 2 == 0:
+#            cantidad += 1
+#            return cantidad
+#        else:
+#            pass
+
+#lista_numeros = cantidad_pares([1,50,502,755,34])
+#print(lista_numeros)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
