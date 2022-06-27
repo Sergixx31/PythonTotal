209c4d77387184e23d0c4d5ffa3cdc9700ae1b40
smartinez <smartinez@zitrointeractive.com>
2022-06-27 13:16:17 +0200

    add file ayuda and funciones

---
+++ Dia5/ayuda.py
@@ -0,0 +1,47 @@
+#dic = {"clave1":100, "clave2":500}
+#a = dic.popitem()
+#print(a)
+#print(dic)
+
+
+
+#texto = ",:_#,,,,,,:::____##Pyt%on_ _Total,,,,,,::#"
+#x = texto.lstrip(",:_#,:_#")
+#print(x)
+
+
+
+#frutas = ["mango", "banana", "cereza", "ciruela", "pomelo"]
+#frutas.insert(3,"naranja")
+#print(frutas)
+
+#marcas_smartphones = {"Samsung", "Xiaomi", "Apple", "Huawei", "LG"}
+#marcas_tv = {"Sony", "Philips", "Samsung", "LG"}
+
+#conjuntos_aislados = marcas_smartphones.isdisjoint(marcas_tv)
+#print(conjuntos_aislados)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
