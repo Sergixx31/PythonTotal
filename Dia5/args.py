082202fa64cbaa9347588b75c3755426e7998b4a
smartinez <smartinez@zitrointeractive.com>
2022-06-28 12:45:46 +0200

    add args

---
+++ Dia5/args.py
@@ -0,0 +1,19 @@
+#def suma(*args):
+#    return sum(args)
+
+
+#print(suma(5,123,5,235,4))
+
+#def suma_cuadrados(*args):
+#    valores = int(args)
+#    return sum(valores ** 2)
+#print(suma_cuadrados(1,2,3))
+
+#nombre = input("Dime tu nombre: ")
+#numero = input("Dime unos numeros: ")
+
+#def numeros_persona(nombre, numeros):
+#    suma = sum(numeros)
+#    print(f"{nombre}, la suma de tus números es {suma}")
+
+#numeros_persona(nombre, numeros)
