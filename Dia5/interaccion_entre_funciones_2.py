9a39a1c974204c882f8352d6b83ed6f813571cba
smartinez <smartinez@zitrointeractive.com>
2022-06-28 08:42:01 +0200

    fix interaccion_entre_funciones_2

--- Dia5/interaccion_entre_funciones_2.py
+++ Dia5/interaccion_entre_funciones_2.py
@@ -1,22 +1,57 @@
-from random import randint
+#from random import randint
+#
+#def lanzar_dados():
+#    numero1 = randint(1,6)
+#    numero2 = randint(1,6)
+#    return numero1, numero2
+#
+#
+#def evaluar_jugada(numero1, numero2):
+#    suma_dados = numero1 + numero2
+#    if suma_dados <= 6:
+#        print(f"La suma de tus dados es {suma_dados}. Lamentable")
+#    elif 6 < suma_dados < 10:
+#        print(f"La suma de tus dados es {suma_dados}. Tienes buenas chances")
+#    elif suma_dados >= 10:
+#        print(f"La suma de tus dados es {suma_dados}. Parece una jugada ganadora")
+#    else:
+#        "ª"
+#
+#
+#dado1, dado2 = lanzar_dados()
+#evaluar_jugada(dado1, dado2)
 
-def lanzar_dados():
-    numero1 = randint(1,6)
-    numero2 = randint(1,6)
-    return numero1, numero2
 
 
-def evaluar_jugada(numero1, numero2):
-    suma_dados = numero1 + numero2
-    if suma_dados <= 6:
-        print(f"La suma de tus dados es {suma_dados}. Lamentable")
-    elif 6 < suma_dados < 10:
-        print(f"La suma de tus dados es {suma_dados}. Tienes buenas chances")
-    elif suma_dados >= 10:
-        print(f"La suma de tus dados es {suma_dados}. Parece una jugada ganadora")
-    else:
-        "ª"
 
 
-dado1, dado2 = lanzar_dados()
-evaluar_jugada(dado1, dado2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
