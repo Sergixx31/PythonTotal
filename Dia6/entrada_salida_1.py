4a8f9876e55e158889c9110ebd43829c182910d2
smartinez <smartinez@zitrointeractive.com>
2022-06-30 10:30:37 +0200

    add archivos

---
+++ Dia6/entrada_salida_1.py
@@ -0,0 +1,43 @@
+mi_archivo = open('Prueba.txt', encoding='cp1252', errors='surrogateescape')
+
+##print(mi_archivo.read())
+#una_linea = mi_archivo.readline()
+##tambien puedes usar metodos de strings
+#print(una_linea.upper())
+#
+#una_linea = mi_archivo.readline()
+##sin el salto de linea
+#print(una_linea.rstrip())
+#
+#una_linea = mi_archivo.readline()
+#print(una_linea)
+#
+#mi_archivo.close()
+
+
+#for l in mi_archivo:
+#   print("Aqui dice: " + l)
+
+#crear lista
+#todas = mi_archivo.readlines()
+#todas = todas.pop()
+#print(todas)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
