afa9891165655d59e975471095c4a3c67fb98363
smartinez <smartinez@zitrointeractive.com>
2022-06-30 14:35:40 +0200

    add pathlib and directorios

---
+++ Dia6/directorios.py
@@ -0,0 +1,65 @@
+import os
+#te muestra la ruta actual
+#ruta = os.getcwd()
+#ruta = os.chdir("/home/sergi/PycharmProjects/PythonTotal/alternativa(directorios)")
+#ruta = os.makedirs("/home/sergi/PycharmProjects/PythonTotal/alternativa(directorios)/otra")
+#ruta = "/home/sergi/PycharmProjects/PythonTotal/Dia6/Prueba.txt"
+
+#nombre de base de elemento
+#elemento = os.path.basename(ruta)
+
+#nombre del directorio
+#elemento = os.path.dirname(ruta)
+
+#muestra la ruta + el archivo en una tupla
+#elemento = os.path.split(ruta)
+#print(elemento)
+
+#eliminar carpeta
+#os.rmdir("/home/sergi/PycharmProjects/PythonTotal/alternativa(directorios)/otra")
+
+#contenido del archivo desde otra ruta
+#otro_archivo = open('/home/sergi/PycharmProjects/PythonTotal/alternativa(directorios)/otro_archivo.txt',"r", encoding='cp1252', errors='surrogateescape')
+#print(otro_archivo.read())
+
+
+
+
+#from pathlib import Path
+
+#carpeta = Path("/home/sergi/PycharmProjects/PythonTotal/alternativa(directorios)")
+#archivo = carpeta / "otro_archivo.txt"
+
+#mi_archivo = open(archivo,"r", encoding='cp1252', errors='surrogateescape')
+#print(mi_archivo.read())
+
+
+
+##########################################################
+#mi_archivo = open('otro_archivo.txt',"r", encoding='cp1252', errors='surrogateescape')
+#print(mi_archivo.read())
+#mi_archivo.close()
+##########################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
