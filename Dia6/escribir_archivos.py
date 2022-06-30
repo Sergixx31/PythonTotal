c76c3736a40fbb912a20d4186bdbd3216fa6aed6
smartinez <smartinez@zitrointeractive.com>
2022-06-30 12:09:43 +0200

    add archivo txt

--- Dia6/escribir_archivos.py
+++ Dia6/escribir_archivos.py
@@ -2,7 +2,7 @@
 #con la a añade al final del todo
 #R de solo lectura, por default es la R
 
-archivo = open('Prueba.txt',"a", encoding='cp1252', errors='surrogateescape')
+#archivo = open('Prueba.txt',"a", encoding='cp1252', errors='surrogateescape')
 #archivo.write("hola ")
 #archivo.write("mundo")
 
@@ -12,16 +12,29 @@ archivo = open('Prueba.txt',"a", encoding='cp1252', errors='surrogateescape')
 #    archivo.writelines(p + "\n")
 
 
-
-archivo.write("Bienvenido")
-
-
+#archivo.write("Bienvenido")
+#archivo.close()
 
 
 
 
+#mi_archivo = open('mi_archivo.txt',"w", encoding='cp1252', errors='surrogateescape')
+#mi_archivo.write("Nuevo texto")
+#mi_archivo = open('mi_archivo.txt',"r", encoding='cp1252', errors='surrogateescape')
+#print(mi_archivo.read())
+#mi_archivo.close()
 
 
 
 
-archivo.close()
+
+
+
+
+
+
+
+
+
+
+
