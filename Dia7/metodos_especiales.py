c90c120ecdcbdd95ce5c6d57065bf212c861ff53
smartinez <sergixx31@gmail.com>
2022-07-06 14:56:25 +0200

    add metodos y proyecto

---
+++ Dia7/metodos_especiales.py
@@ -0,0 +1,72 @@
+#__init__ inicializar atributos de una clase nueva
+#__mro__ orden en que se realiza la busqueda de metodos heredables
+
+class CD:
+    def __init__(self, autor, titulo, canciones):
+        self.autor = autor
+        self.titulo = titulo
+        self.canciones = canciones
+
+    def __str__(self):
+        return f"Album: {self.titulo} de {self.autor}"
+
+    def __len__(self):
+        return self.canciones
+
+    def __del__(self):
+        print("Se ha eliminado el cd")
+
+mi_cd = CD("pinkfloyd", "the wall", 24)
+
+del mi_cd
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Libro():
+    def __init__(self, titulo, autor, cantidad_paginas):
+        self.titulo = titulo
+        self.autor = autor
+        self.cantidad_paginas = cantidad_paginas
+
+    def __str__(self):
+        return f'"{self.titulo}", de {self.autor}'
+
+
+class Libro():
+    def __init__(self, titulo, autor, cantidad_paginas):
+        self.titulo = titulo
+        self.autor = autor
+        self.cantidad_paginas = cantidad_paginas
+
+    def __len__(self):
+        return self.cantidad_paginas
+
+
+class Libro():
+    def __init__(self, titulo, autor, cantidad_paginas):
+        self.titulo = titulo
+        self.autor = autor
+        self.cantidad_paginas = cantidad_paginas
+
+    def __del__(self):
+        print(f'Libro eliminado')
+
+
+
+
+
